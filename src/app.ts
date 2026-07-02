cc23d94db8550b4ff7b545ea31c76a425acb9d60
Dylan10123 <dbds.101203@gmail.com>
2026-07-02 10:06:03 +0200

    feat: Añadidas las rutas de login y register a la API. Creado un archivo de rutas para realizar test.

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import User from './models/User.js';
 import Log from './models/Log.js';
 import express from 'express';
 import dotenv from 'dotenv';
+import router from './routes/auth.routes.js';
 
 dotenv.config();
 connectDB();
@@ -12,6 +13,8 @@ const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
+
+app.use('/auth', router);
 
 app.listen(PORT, () =>
   console.log(`Servidor ejecutandose en el puerto ${PORT}`),
